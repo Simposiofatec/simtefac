981433c239deb651c8397a5cd284fa23ddb2609d
KaikAp <Kaik.Silva4@outlook.com>
2025-04-21 16:07:09 -0300

    style: melhorias visuais e removido alguns imports invalidos

--- Front-end/src/pages/certificado/Certificado.page.tsx
+++ Front-end/src/pages/certificado/Certificado.page.tsx
@@ -1,17 +1,14 @@
 import React, { useContext, useEffect, useState } from "react";
-import { saveAs } from "file-saver";
-import { jsPDF } from "jspdf";
-import Mammoth from "mammoth";
 import { UserContext } from "../../App";
 import { User } from "../../models/user.model";
 import { GetCertificado, GetPresencas, GetPresencaCertificado } from "../../services/certificate.service";
 
+
 export function CertificadoPdf() {
-  const [file, setFile] = useState<File | null>(null);
   const user = useContext(UserContext);
   const [usuario, setUsuario] = useState<User | null>(null);
   const [presencas, setPresencas] = useState<number | null>(null);
-
+  const [pdfUrl, setPdfUrl] = useState<string | null>(null);
   /**
  * @summary Resgata o usuario logado e insere ele no usuario
  * @description Isso foi feito para evitar erro de undefined que ocorria
@@ -56,6 +53,7 @@ presencasFunction();
       const certificadoBlob = await GetCertificado(usuario.name);
   
       const url = window.URL.createObjectURL(certificadoBlob);
+      setPdfUrl(url);
       const link = document.createElement('a');
       link.href = url;
       link.setAttribute('download', 'certificado.pdf');
@@ -73,12 +71,31 @@ presencasFunction();
   
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-6">
-      <h1 className="text-2xl font-bold mb-4">Gerador de Certificado PDF</h1>
-
-      <button onClick={processWordFile} className="bg-blue-500 text-white px-4 py-2 rounded">
+    <div className="flex flex-col items-center justify-center min-h-screen mt-4">
+      <div className="max-w-xl text-center">
+        <h1 className="text-3xl font-bold text-gray-800 mb-1">Gerar Certificado</h1>
+        <div className="mt-2 text-sm text-gray-900 italic">
+          {!pdfUrl ? (
+            <p>Você deve possuir mais de 75% de presença para que o certificado seja gerado</p>):
+            <p>Certificado gerado com sucesso ✔️</p>
+          }
+        </div>
+      </div>
+      {pdfUrl && (
+        // eslint-disable-next-line jsx-a11y/iframe-has-title
+        <iframe
+          src={pdfUrl}
+          className="mt-8 w-full max-w-4xl h-[80vh] border rounded shadow"
+        />
+      )}
+      <div className="mt-2">
+      <button 
+        onClick={processWordFile} 
+        className="bg-blue-600 hover:bg-blue-700 text-white font-semibold px-6 py-3 rounded-lg shadow transition duration-300">
         Gerar PDF
       </button>
+      </div>
     </div>
   );
+  
 }
